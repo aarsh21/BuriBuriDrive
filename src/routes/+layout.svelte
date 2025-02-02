734abe31c316b0719fb22d16e3a2aa5a458f6f54
aarsh21 <aarshpersonal21@gmail.com>
2025-02-02 16:20:27 +0530

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import '../app.css';
+	let { children } = $props();
+</script>
+
+<div
+	class="flex min-h-screen w-full flex-1 flex-col items-center justify-center bg-gradient-to-r from-zinc-500 via-stone-600 to-zinc-900 text-zinc-50"
+>
+	{@render children()}
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,1 @@
+<h1 class="bg-linear-to-r from-zinc-500 via-stone-600 to-zinc-900">Buri Buri Zaimon</h1>
